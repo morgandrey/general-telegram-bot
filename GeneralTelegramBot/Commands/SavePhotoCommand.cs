c944a13154e777b375ea2bc77e8564dd6b88a705
Andrey <morgunof-andrey@yandex.ru>
2022-06-18 19:51:09 +0400

    Remove Yandex disk photo upload

--- GeneralTelegramBot/Commands/SavePhotoCommand.cs
+++ GeneralTelegramBot/Commands/SavePhotoCommand.cs
@@ -2,8 +2,6 @@ using GeneralTelegramBot.DataAccess.Models;
 using GeneralTelegramBot.DataAccess.Repository;
 using GeneralTelegramBot.Utils;
 using Telegram.Bot;
-using YandexDisk.Client.Clients;
-using YandexDisk.Client.Http;
 using TelegramMessage = Telegram.Bot.Types.Message;
 using SystemFile = System.IO.File;
 
@@ -12,12 +10,6 @@ namespace GeneralTelegramBot.Commands;
 public class SavePhotoCommand
 {
     private static readonly UnitOfWork unitOfWork = new UnitOfWork();
-    private const string YandexDiskUrl = "https://disk.yandex.ru/d/QUVXM648JvB5cQ";
-
-    private static async Task UploadToYandexDisk(string diskPath, string localPath, CancellationToken cancellationToken)
-    {
-        await new DiskHttpApi(Constants.YandexDiskToken).Files.UploadFileAsync(diskPath, false, localPath, cancellationToken);
-    }
 
     public static async Task Execute(ITelegramBotClient botClient, TelegramMessage message, string image, CancellationToken cancellationToken)
     {
@@ -32,12 +24,9 @@ public class SavePhotoCommand
             await outputFileStream.DisposeAsync();
 
             await SavePhotoToDb(message, tempFilePath);
-            //await UploadToYandexDisk($"disk:/Фотки/{DateTime.Now:MM-dd-yyyy;hh-mm-sstt}",
-            //    tempFilePath,
-            //    cancellationToken: cancellationToken);
             SystemFile.Delete(tempFilePath);
             await botClient.SendTextMessageAsync(chatId,
-                $"Photos saved successfully!\nUrl: {YandexDiskUrl}",
+                "Photos saved successfully!",
                 cancellationToken: cancellationToken);
         }
         catch (Exception ex)
